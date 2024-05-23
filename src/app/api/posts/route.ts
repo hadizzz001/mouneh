e8415e0dadc5a44e1b61fead4b2560718b08a1b4
Your Name <you@example.com>
2024-05-23 13:42:26 +0300

    first commit

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,6 +2,8 @@
 import prisma from "../../../../prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function main() {
     try {
         await prisma.$connect();
